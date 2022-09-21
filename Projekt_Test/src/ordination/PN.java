2bbdfeb4a545e51402c3c296425f55e11b4f9d26
SidselAva <sidselkirk@live.dk>
2022-09-21 11:52:15 +0200

    Changes made to PN class

--- Projekt_Test/src/ordination/PN.java
+++ Projekt_Test/src/ordination/PN.java
@@ -1,41 +1,56 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PN {
+public class PN extends Ordination {
 
     private double antalEnheder;
+    private Ordination ordination;
+    private List<LocalDate> dateList = new ArrayList<>();
+
+    public PN(LocalDate startDen, LocalDate slutDen, Patient patient) {
+        super(startDen, slutDen, patient);
+    }
 
     /**
      * Registrerer at der er givet en dosis paa dagen givesDen
      * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
      * Retrurner false ellers og datoen givesDen ignoreres
+     *
      * @param givesDen
      * @return
      */
     public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        return false;   
+        dateList.add(givesDen);
+        return givesDen.isAfter(ordination.getStartDen().minusDays(1))
+                && givesDen.isBefore(ordination.getSlutDen().plusDays(1));
     }
 
     public double doegnDosis() {
-        // TODO
+        double sum = 0;
+
         return 0.0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 
 
     public double samletDosis() {
-        // TODO
         return 0.0;
     }
 
     /**
      * Returnerer antal gange ordinationen er anvendt
+     *
      * @return
      */
     public int getAntalGangeGivet() {
-        // TODO
-        return-1;
+        return dateList.size();
     }
 
     public double getAntalEnheder() {
@@ -43,3 +58,4 @@ public class PN {
     }
 
 }
+
