98d6d5ecf66a240a4b6b847ca987e33c3b377682
SidselAva <sidselkirk@live.dk>
2022-09-22 16:17:48 +0200

    PN test

--- Projekt_Test/test/ordination/PNTest.java
+++ Projekt_Test/test/ordination/PNTest.java
@@ -1,34 +1,236 @@
 package ordination;
 
+import org.junit.jupiter.api.*;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PNTest {
 
-    @org.junit.jupiter.api.Test
-    void givDosis() {
+    PN pn;
+    Patient patient;
+
+    @BeforeEach
+    void setup() {
+        this.patient = new Patient("123494-7890", "Donna Summer", 66.5);
+        LocalDate start = LocalDate.of(2022, 02, 03);
+        LocalDate slut = LocalDate.of(2022, 02, 10);
+        int antalEnheder = 10;
+        this.pn = new PN(start, slut, patient, antalEnheder);
+
     }
 
-    @org.junit.jupiter.api.Test
-    void doegnDosis() {
+    @Test
+    void TC1_givDosis_dato_2022_02_03() {
+        // ARRANGE
+        int expectedOutput = 10;
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 03));
+
+        //
+        assertEquals(expectedOutput, pn.samletDosis());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void TC2_givDosis_dato_2022_02_08() {
+        // ARRANGE
+        int expectedOutput = 10;
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 8));
+
+        //
+        assertEquals(expectedOutput, pn.samletDosis());
+    }
+
+    @Test
+    void TC3_givDosis_dato_2022_02_04() {
+        // ARRANGE
+        int expectedOutput = 10;
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 4));
+
+        // ASSERT
+        assertEquals(expectedOutput, pn.samletDosis());
+    }
+
+    @Test
+    void TC4_givDosis_dato_2022_02_01() {
+        // ARRANGE
+        int expectedOutput = 0;
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 01));
+
+        // ASSERT
+        assertEquals(expectedOutput, pn.samletDosis());
+    }
+
+
+    @Test
+    void TC5_givDosis_dato_2022_02_12() {
+        // ARRANGE
+        int expectedOutput = 0;
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 12));
+
+        // ASSERT
+        assertEquals(expectedOutput, pn.samletDosis());
+    }
+
+
+    @Test
+    void TC1_doegnDosis_2_Gange_1_doegn() {
+
+        // ARRANGE
+        double expectedOutput = 20;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 04));
+        pn.givDosis(LocalDate.of(2022, 02, 05));
+
+        // ASSERT
+        assertEquals(df.format(expectedOutput), df.format(pn.doegnDosis()));
+
+
+    }
+
+    @Test
+    void TC2_doegnDosis_15_Gange_6_doegn() {
+
+        // ARRANGE
+        double expectedOutput = 25;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 04));
+        pn.givDosis(LocalDate.of(2022, 02, 05));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,6));
+        pn.givDosis(LocalDate.of(2022,02,7));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,9));
+        pn.givDosis(LocalDate.of(2022,02,9));
+        pn.givDosis(LocalDate.of(2022,02,9));
+        pn.givDosis(LocalDate.of(2022,02,10));
+
+
+        // ASSERT
+        assertEquals(df.format(expectedOutput), df.format(pn.doegnDosis()));
+
+
+    }
+
+    @Test
+    void TC3_doegnDosis_7_Gange_3_doegn() {
+
+        // ARRANGE
+        double expectedOutput = 23.33;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022, 02, 04));
+        pn.givDosis(LocalDate.of(2022, 02, 05));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,6));
+        pn.givDosis(LocalDate.of(2022,02,7));
+
+        // ASSERT
+        assertEquals(df.format(expectedOutput), df.format(pn.doegnDosis()));
+
+
+    }
+
+    @Test
     void getType() {
     }
 
-    @org.junit.jupiter.api.Test
-    void samletDosis() {
+    @Test
+    void TC1_samletDosis_2_anvendt_15_enheder() {
+
+        // ARRANGE
+        LocalDate datoGivet = LocalDate.of(2022, 02, 05);
+        int expectedOutput = 30;
+        pn.setAntalEnheder(15);
+
+        // ACT
+        pn.givDosis(datoGivet);
+        pn.givDosis(datoGivet);
+
+        // ASSERT
+        assertEquals(expectedOutput,pn.samletDosis());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void TC2_samletDosis_12_anvendt_4_enheder() {
+
+        // ARRANGE
+        LocalDate datoGivet = LocalDate.of(2022, 02, 05);
+        int expectedOutput = 48;
+        pn.setAntalEnheder(4);
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,6));
+        pn.givDosis(LocalDate.of(2022,02,7));
+        pn.givDosis(LocalDate.of(2022,02,7));
+        pn.givDosis(LocalDate.of(2022,02,7));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,8));
+        pn.givDosis(LocalDate.of(2022,02,9));
+        pn.givDosis(LocalDate.of(2022,02,10));
+        pn.givDosis(LocalDate.of(2022,02,10));
+
+        // ASSERT
+        assertEquals(expectedOutput,pn.samletDosis());
+
+
+    }
+
+    @Test
+    void TC3_samletDosis_5_anvendt_8_enheder() {
+
+        // ARRANGE
+        LocalDate datoGivet = LocalDate.of(2022, 02, 05);
+        int expectedOutput = 40;
+        pn.setAntalEnheder(8);
+
+        // ACT
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,5));
+        pn.givDosis(LocalDate.of(2022,02,6));
+        pn.givDosis(LocalDate.of(2022,02,7));
+        pn.givDosis(LocalDate.of(2022,02,7));
+
+
+        // ASSERT
+        assertEquals(expectedOutput,pn.samletDosis());
+
+    }
+
+
+    @Test
     void getAntalGangeGivet() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAntalEnheder() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setAntalEnheder() {
     }
 }
